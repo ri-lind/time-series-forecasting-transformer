7e462e5b1eb27d08c6e3c360e18295b0b0ab0eb8
rilind <lindiharizaj@gmail.com>
2025-02-16 11:45:17 +0100

    added mase for evaluation

--- run_eval.py
+++ run_eval.py
@@ -16,8 +16,8 @@ from time_moe.datasets.benchmark_dataset import BenchmarkEvalDataset
 
 
 def setup_nccl(rank, world_size, master_addr='127.0.0.1', master_port=9899):
-    dist.init_process_group("nccl", init_method='tcp://{}:{}'.format(master_addr, master_port), rank=rank,
-                            world_size=world_size)
+    dist.init_process_group("nccl", init_method='tcp://{}:{}'.format(master_addr, master_port),
+                            rank=rank, world_size=world_size)
 
 
 def count_num_tensor_elements(tensor):
@@ -37,7 +37,7 @@ class SumEvalMetric:
         self.value += self._calculate(preds, labels, **kwargs)
 
     def _calculate(self, preds, labels, **kwargs):
-        pass
+        raise NotImplementedError
 
 
 class MSEMetric(SumEvalMetric):
@@ -48,6 +48,32 @@ class MSEMetric(SumEvalMetric):
 class MAEMetric(SumEvalMetric):
     def _calculate(self, preds, labels, **kwargs):
         return torch.sum(torch.abs(preds - labels))
+
+
+class MASEMetric(SumEvalMetric):
+    def __init__(self, name, init_val: float = 0.0):
+        super().__init__(name, init_val)
+        self.num_sequences = 0
+
+    def push(self, preds, labels, **kwargs):
+        # For MASE, we count the number of sequences (i.e. batch size)
+        batch_size = labels.shape[0]
+        self.num_sequences += batch_size
+        self.value += self._calculate(preds, labels, **kwargs)
+
+    def _calculate(self, preds, labels, **kwargs):
+        # Assume labels has shape (batch, prediction_length)
+        batch_size, pred_len = labels.shape
+        abs_errors = torch.abs(preds - labels)         # shape: (batch, prediction_length)
+        mae_per_seq = torch.mean(abs_errors, dim=1)      # (batch,)
+        # Compute naive forecast error per sequence as the mean absolute difference
+        # between consecutive values in the ground-truth labels.
+        if pred_len < 2:
+            naive_error = torch.ones(batch_size, device=labels.device)
+        else:
+            naive_error = torch.mean(torch.abs(labels[:, 1:] - labels[:, :-1]), dim=1)
+        mase = mae_per_seq / (naive_error + 1e-8)        # (batch,)
+        return torch.sum(mase)
 
 
 class TimeMoE:
@@ -115,11 +141,11 @@ def evaluate(args):
         device = 'cpu'
         is_dist = False
 
-    # evaluation
-    metric_list = [
-        MSEMetric(name='mse'),
-        MAEMetric(name='mae'),
-    ]
+    # evaluation metrics: note that for MSE and MAE we count tokens, but for MASE we count sequences.
+    mse_metric = MSEMetric(name='mse')
+    mae_metric = MAEMetric(name='mae')
+    mase_metric = MASEMetric(name='mase')
+    metric_list = [mse_metric, mae_metric, mase_metric]
 
     model = TimeMoE(
         args.model,
@@ -147,22 +173,26 @@ def evaluate(args):
         drop_last=False,
     )
 
-    acc_count = 0
+    acc_count = 0  # for MSE and MAE (total number of tokens)
     with torch.no_grad():
         for idx, batch in enumerate(tqdm(test_dl)):
             preds, labels = model.predict(batch)
 
-            for metric in metric_list:
-                metric.push(preds, labels)
+            mse_metric.push(preds, labels)
+            mae_metric.push(preds, labels)
+            mase_metric.push(preds, labels)
 
             acc_count += count_num_tensor_elements(preds)
 
+    # For MSE and MAE, average error per token; for MASE, average error per sequence.
     ret_metric = {}
-    for metric in metric_list:
-        ret_metric[metric.name] = metric.value / acc_count
+    ret_metric[mse_metric.name] = mse_metric.value / acc_count
+    ret_metric[mae_metric.name] = mae_metric.value / acc_count
+    ret_metric[mase_metric.name] = mase_metric.value / mase_metric.num_sequences
+
     print(f'{rank} - {ret_metric}')
 
-    metric_tensors = [metric.value for metric in metric_list] + [acc_count]
+    metric_tensors = [mse_metric.value, mae_metric.value, mase_metric.value, acc_count]
     if is_dist:
         stat_tensor = torch.tensor(metric_tensors).to(model.device)
         gathered_results = [torch.zeros_like(stat_tensor) for _ in range(world_size)]
@@ -178,11 +208,11 @@ def evaluate(args):
             'context_length': args.context_length,
             'prediction_length': args.prediction_length,
         }
-
         count = all_stat[-1]
-        for i, metric in enumerate(metric_list):
-            val = all_stat[i] / count
-            item[metric.name] = float(val.cpu().numpy())
+        # Use appropriate counts for each metric
+        item[mse_metric.name] = float(all_stat[0] / count)
+        item[mae_metric.name] = float(all_stat[1] / count)
+        item[mase_metric.name] = float(all_stat[2] / mase_metric.num_sequences)
         logging.info(item)
 
 
@@ -199,7 +229,6 @@ if __name__ == '__main__':
         type=str,
         help='Benchmark data path'
     )
-
     parser.add_argument(
         '--batch_size', '-b',
         type=int,
